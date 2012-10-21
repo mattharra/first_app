7c8606594c54f50742ba43643bd046ed607e8280
Matthew Harra <mattharra@gmail.com>
2012-10-20 23:12:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acgs::Application.config.secret_token = '7441494b0d94233d60464fb0b2112963519fdc116a98a289dde620a1eb8af09ca230c0272f63863bea1b0b97a0a0e9a1d9240e24e9a049bbb01f0925eba85c58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Acgs::Application.config.session_store :cookie_store, key: '_acgs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Acgs::Application.config.session_store :active_record_store
